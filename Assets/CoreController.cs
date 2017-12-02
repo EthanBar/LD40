07beb2d6e9f0b5aea9bf127bb3c3b3da8f457936
Ethan Barker <ethanbarker0@gmail.com>
2017-12-01 23:31:29 -0800

    Basic movement

--- Assets/CoreController.cs
+++ Assets/CoreController.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CoreController : MonoBehaviour {
+	public float speedMulti;
+	public float maxSpeed;
+	public float rotSpeed;
+	private Rigidbody2D rb2d;
 
 	// Use this for initialization
 	void Start () {
-		
+		rb2d = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (Input.GetKey(KeyCode.A)) {
+			rb2d.AddTorque(-rotSpeed);
+		}
+		if (Input.GetKey(KeyCode.D)) {
+			rb2d.AddTorque(rotSpeed);
+		}
 		
+		if (Input.GetKey(KeyCode.Space) && rb2d.velocity.magnitude < maxSpeed) {
+			rb2d.AddForce(transform.right * speedMulti);
+		}
 	}
 }
